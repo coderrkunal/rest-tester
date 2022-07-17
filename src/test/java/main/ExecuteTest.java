c5400479b7b75b9eefccb87aee3b5d94e75791c0
coderrkunal <kunalcoderr@gmail.com>
2022-07-17 14:50:53 +0530

    read test file from rsource

--- src/test/java/main/ExecuteTest.java
+++ src/test/java/main/ExecuteTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.testng.Assert.assertTrue;
@@ -34,8 +35,12 @@ public class ExecuteTest {
   @Test public void testProcessWithPropsFile() {
     //Test asserts
     Execute execute = null;
+    String resourceName = "temp.txt";
     try {
-      execute = new Execute("/Users/kunal/temp.txt");
+      ClassLoader classLoader = getClass().getClassLoader();
+      File file = new File(classLoader.getResource(resourceName).getFile());
+      String absolutePath = file.getAbsolutePath();
+      execute = new Execute(absolutePath);
     } catch (IOException e) {
       Assert.fail("testProcessWithPropsFile Failed with : " + e.getMessage());
       e.printStackTrace();
